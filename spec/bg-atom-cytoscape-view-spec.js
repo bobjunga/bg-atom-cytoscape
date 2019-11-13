4239cc252a3eaeb0e059cbbc031fd3f6879e3fef
Robert E. Griffith <bobg@avature.net>
2019-11-13 11:32:19 -0500

    initial generated project

---
+++ spec/bg-atom-cytoscape-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import BgAtomCytoscapeView from '../lib/bg-atom-cytoscape-view';
+
+describe('BgAtomCytoscapeView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
